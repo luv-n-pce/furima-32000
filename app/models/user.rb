e030e57753f1bb5909fcb2adce8baf9f992e0bb8
luv-n-pce <loves2peace.1945@gmail.com>
2020-11-25 17:48:59 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, message: "includes both letters and numbers"}
+  validates :password, format: { with: /\A[a-z0-9]+\z/, message: "includes both letters and numbers"}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
