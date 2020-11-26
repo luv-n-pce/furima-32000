937b3b178bb4b270d6ef41f178dde53da7f560ec
luv-n-pce <loves2peace.1945@gmail.com>
2020-11-26 15:43:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence :true, length: { maximum: 40 }
+  validates :introduction, presence :true, length: { maximum: 1000 }
+  validates :category_id, presence :true
+  validates :item_condition_id, presence :true
+  validates :postage_payer_id, presence :true
+  validates :prefecture_code_id, presence :true
+  validates :preparation_day_id, presence :true
+  validates :price, presence :true
+  
 end
