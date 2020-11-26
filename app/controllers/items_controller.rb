5dff37f2859395e7a0565726c648895b9cf15992
luv-n-pce <loves2peace.1945@gmail.com>
2020-11-26 16:43:34 +0900

    各モデルの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,5 @@ class ItemsController < ApplicationController
   def index
   end
 
-
-
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :prefecture_code
+  belongs_to :preparation_day
+
   validates :name, presence :true, length: { maximum: 40 }
   validates :introduction, presence :true, length: { maximum: 1000 }
-  validates :category_id, presence :true
-  validates :item_condition_id, presence :true
-  validates :postage_payer_id, presence :true
-  validates :prefecture_code_id, presence :true
-  validates :preparation_day_id, presence :true
+  validates :category_id, presence :true, numericality: { other_than: 1 } 
+  validates :item_condition_id, presence :true, numericality: { other_than: 1 } 
+  validates :postage_payer_id, presence :true, numericality: { other_than: 1 } 
+  validates :prefecture_code_id, presence :true, numericality: { other_than: 1 } 
+  validates :preparation_day_id, presence :true, numericality: { other_than: 1 } 
   validates :price, presence :true
   
 end
