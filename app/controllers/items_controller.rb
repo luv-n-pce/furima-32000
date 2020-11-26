d110ae6ab1501eb8a4125db1015407267d44873c
luv-n-pce <loves2peace.1945@gmail.com>
2020-11-26 18:25:14 +0900

    ルーティング、コントローラー、ビューの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,25 @@ class ItemsController < ApplicationController
 
 
   def index
+    
+  end 
+
+  def new
+    @item = Item.new
   end
 
-  
+  def create
+    @pitem = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture_code
   belongs_to :preparation_day
 
-  validates :name, presence :true, length: { maximum: 40 }
-  validates :introduction, presence :true, length: { maximum: 1000 }
-  validates :category_id, presence :true, numericality: { other_than: 1 } 
-  validates :item_condition_id, presence :true, numericality: { other_than: 1 } 
-  validates :postage_payer_id, presence :true, numericality: { other_than: 1 } 
-  validates :prefecture_code_id, presence :true, numericality: { other_than: 1 } 
-  validates :preparation_day_id, presence :true, numericality: { other_than: 1 } 
-  validates :price, presence :true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :introduction, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :item_condition_id, presence: true, numericality: { other_than: 1 } 
+  validates :postage_payer_id, presence: true, numericality: { other_than: 1 } 
+  validates :prefecture_code_id, presence: true, numericality: { other_than: 1 } 
+  validates :preparation_day_id, presence: true, numericality: { other_than: 1 } 
+  validates :price, presence: true
   
 end
