fe7c11fedd27e1c437b067cba007c24aa44b4d85
luv-n-pce <loves2peace.1945@gmail.com>
2020-11-27 11:02:23 +0900

    エラーメッセージの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture_code
   belongs_to :preparation_day
 
+  validates :image, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :introduction, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { other_than: 1 } 
-  validates :item_condition_id, presence: true, numericality: { other_than: 1 } 
-  validates :postage_payer_id, presence: true, numericality: { other_than: 1 } 
-  validates :prefecture_code_id, presence: true, numericality: { other_than: 1 } 
-  validates :preparation_day_id, presence: true, numericality: { other_than: 1 } 
-  validates :price, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "Select" } 
+  validates :item_condition_id, presence: true, numericality: { other_than: 1, message: "Select" } 
+  validates :postage_payer_id, presence: true, numericality: { other_than: 1, message: "Select" } 
+  validates :prefecture_code_id, presence: true, numericality: { other_than: 1, message: "Select" } 
+  validates :preparation_day_id, presence: true, numericality: { other_than: 1, message: "Select" } 
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "Half-width number" }
   
 end
